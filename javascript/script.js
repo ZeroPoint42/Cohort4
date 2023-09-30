9eae85d097f8c6cb5d1283945c23aa8b3d9de00d
Emanate <info@emanatedesigns.com>
2023-09-30 09:22:54 -0700

    added js slide and fade effect

--- javascript/script.js
+++ javascript/script.js
@@ -45,3 +45,26 @@ function toggleMode() {
     emailToggle.classList.toggle('text-black');
 };
 modeToggle.addEventListener('click', toggleMode);
+
+// fade and slide on scroll
+const items = document.querySelectorAll('.card:not(:first-child)');
+
+const options = {
+  threshold: 0.5
+}
+
+function addSlideIn(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('slide-in');
+    } else { // this else part is not compulsory
+      entry.target.classList.remove('slide-in');
+    }
+  });
+}
+
+const observer = new IntersectionObserver(addSlideIn, options);
+
+items.forEach(item => {
+  observer.observe(item);
+});
